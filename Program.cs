c7ec52555e17319e12c507828bb2adbe793d8a25
Pivomaru <spardasenpai@gmail.com>
2022-06-07 21:19:31 +0300

    DZ2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
-﻿int a = 4;
-int b = 7;
+﻿// Напишите программу, которая принимает на
+//вход три числа и выдаёт максимальное из этих чисел.
+int a = 44;
+int b = 5;
+int c = 78;
 int max = a;
-if(b > max ) max = b;
+if(b > max) max = b;
+if(c > max) max = c;
 Console.WriteLine(max);
