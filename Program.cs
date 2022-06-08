66e4f1831ae59369d1b1e3730daa9b0d23214535
Pivomaru <spardasenpai@gmail.com>
2022-06-08 22:06:36 +0300

    Added Min

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
-﻿// Напишите программу, которая принимает на
-//вход три числа и выдаёт максимальное из этих чисел.
-int a = 44;
-int b = 5;
-int c = 78;
-int max = a;
-if(b > max) max = b;
-if(c > max) max = c;
-Console.WriteLine(max);
+﻿
+        // Напишите программу, которая принимает на
+        //вход три числа и выдаёт максимальное из этих чисел.
+        int a = 44;
+        int b = 5;
+        int c = 78;
+        int max = a;
+        int min = a;
+        if (b > max) max = b;
+        if (c > max) max = c;
+        if (b < min) min = b;
+        if (c < min) min = c;
+        Console.WriteLine("Max: " + max);
+        Console.WriteLine("Min: " + min);
+    
